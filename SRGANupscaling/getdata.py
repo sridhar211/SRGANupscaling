0903b1bf3b9f582f6caad1f3a95bc46090d5c4a5
melissa siddle <melissa.sid98@gmail.com>
2022-07-12 17:09:18 +0200

    notebook

--- SRGANupscaling/getdata.py
+++ SRGANupscaling/getdata.py
@@ -48,21 +48,6 @@ def download_blob(bucket_name=BUCKET_NAME, prefix='datasets/kaggle100-original/H
 #     return
 
 
-def compute_distance(df,
-                     start_lat="pickup_latitude",
-                     start_lon="pickup_longitude",
-                     end_lat="dropoff_latitude",
-                     end_lon="dropoff_longitude"):
-    lat_1_rad, lon_1_rad = np.radians(df[start_lat].astype(float)), np.radians(df[start_lon].astype(float))
-    lat_2_rad, lon_2_rad = np.radians(df[end_lat].astype(float)), np.radians(df[end_lon].astype(float))
-    dlon = lon_2_rad - lon_1_rad
-    dlat = lat_2_rad - lat_1_rad
-
-    a = np.sin(dlat / 2.0) ** 2 + np.cos(lat_1_rad) * np.cos(lat_2_rad) * np.sin(dlon / 2.0) ** 2
-    c = 2 * np.arcsin(np.sqrt(a))
-    return 6371 * c
-
-
 def preprocess(df):
     """method that pre-process the data"""
     df["distance"] = compute_distance(df)
