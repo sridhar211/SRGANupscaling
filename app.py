8754a5f3ba9bd1ea94e86807da57fa172d2d333a
Obakanyinsola Adegun <lifeofakanji@gmail.com>
2022-07-15 14:28:35 +0100

    frontend

--- app.py
+++ app.py
@@ -9,14 +9,33 @@ from SRGANupscaling.main import super_resolution
 
 # with open('style.css') as f:
 #     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+st.set_page_config(
+    page_title="Pixel Perfect",
+    page_icon="💫",
+    layout="centered",
+    initial_sidebar_state="auto",
+)
 
-st.header("Pixel Perfect")
-st.write("Upscale and enhance any image by using our SRGAN model.")
+
+# st.header("Pixel Perfect")
+main_image = Image.open('static/main_banner.png')
+st.image(main_image,use_column_width='auto')
+st.title("Upscale and enhance any image by using our SRGAN model.")
 st.write("It can used for anything! From preserving old media material to \
          enhancing a microscope’s view, or identifying an individual in CCTV - \
          super-resolution’s impact is widespread and extremely evident.")
 
-uploaded_file = st.file_uploader("Choose an image...")
+
+
+st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+
+st.info('✨ Supports all popular image formats 📷 - PNG, JPG, BMP 😉')
+uploaded_file = st.file_uploader("Upload Image 🚀", type=["png","jpg","bmp","jpeg"])
+
+def download_success():
+    st.balloons()
+    st.success('✅ Download Successful !!')
+
 
 if uploaded_file is not None:
     col1, col2 = st.columns([1,1])
@@ -48,8 +67,12 @@ if uploaded_file is not None:
       file_name="imagename.png",
       mime="image/jpeg",
       )
+else:
+    st.warning('⚠ Please upload your Image file 😯')
+
 
 with st.expander("How does it work?"):
      st.write("""
          To upscale your image, we use a SRGAN model to super-resolutionise your image with minimal information distortion.
      """)
+st.markdown("<br><hr><center>Enjoy ❤️</center><hr>", unsafe_allow_html=True)
