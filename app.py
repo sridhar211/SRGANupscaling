a3b385239e842eae701357ab9258a2d48747ab95
Obakanyinsola Adegun <lifeofakanji@gmail.com>
2022-07-18 15:31:09 +0100

    frontend

--- app.py
+++ app.py
@@ -8,15 +8,36 @@ from io import BytesIO
 from SRGANupscaling.main import super_resolution
 import time
 from scipy import ndimage, misc
+import json
+import requests
+from streamlit_option_menu import option_menu
 
-# with open('style.css') as f:
-#     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+CSS = """
+h1 {
+    color: #C18C8C;
+}
+h2 {
+    color: #C18C8C;
+}
+.css-po3vlj {
+    color: #C18C8C;
+    border-radius: 30%;
+}
+"""
+
 st.set_page_config(
     page_title="Pixel Perfect",
     page_icon="💫",
     layout="centered",
     initial_sidebar_state="auto",
 )
+
+selected = option_menu(
+    menu_title = "Compare",
+    options=["Before","After"],
+)
+
 with st.sidebar:
     st.title("Upscale and enhance any image by using our SRGAN model.")
     st.write("It can used for anything! From preserving old media material to \
@@ -39,7 +60,6 @@ st.image(main_image,use_column_width='auto')
 #          super-resolution’s impact is widespread and extremely evident.")
 
 
-
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
 # st.info("✨ It can used for anything! From preserving old media material to \
@@ -48,26 +68,39 @@ st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', uns
 
 uploaded_file = st.file_uploader("Upload Image 🚀", type=["png","jpg","bmp","jpeg"])
 
+src_image = load_image(uploaded_file)
+image = Image.open(uploaded_file)
 
 
 
-
-if uploaded_file is not None:
-    col1, col2 = st.columns([1,1])
-
-    #src_image = load_image(uploaded_file)
-    image = Image.open(uploaded_file)
-
-    with col1:
+if selected == "Before":
         st.markdown("---")
         st.image(image, caption='Input Image', use_column_width=True)
 
-    #st.write(os.listdir())
-
-    with col2:
+if selected == "After":
         im = super_resolution(image)
         st.markdown("---")
         st.image(im,  caption='Output Image', use_column_width=True)
+
+        rgb_im = im.convert('RGB')
+        buf = BytesIO()
+        rgb_im.save(buf, format="JPEG")
+        byte_im = buf.getvalue()
+
+# if uploaded_file is not None:
+#     col1, col2 = st.columns([1,1])
+
+
+#     with col1:
+#         st.markdown("---")
+#         st.image(image, caption='Input Image', use_column_width=True)
+
+#     #st.write(os.listdir())
+
+#     with col2:
+#         im = super_resolution(image)
+#         st.markdown("---")
+#         st.image(im,  caption='Output Image', use_column_width=True)
 
 
 
@@ -76,21 +109,21 @@ if uploaded_file is not None:
     # if im.mode in ("RGBA", "P"):
     #     im = im.convert("RGB")
 
-    rgb_im = im.convert('RGB')
-    buf = BytesIO()
-    rgb_im.save(buf, format="JPEG")
-    byte_im = buf.getvalue()
+    # rgb_im = im.convert('RGB')
+    # buf = BytesIO()
+    # rgb_im.save(buf, format="JPEG")
+    # byte_im = buf.getvalue()
 
-    if st.download_button(
-      label="Download Image ",
-      data=byte_im,
-      file_name=str("super " + uploaded_file.name),
-      mime="image/jpeg",
+if st.download_button(
+    label="Download Image ",
+    data=byte_im,
+    file_name=str("super " + uploaded_file.name),
+    mime="image/jpeg",
 
-      ):
+    ):
 
-        st.balloons()
-        st.success('✅ Download Successful !!')
+    st.balloons()
+    st.success('✅ Download Successful !!')
 
 
 
